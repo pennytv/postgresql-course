fd54faa2116ab439c959d9e77d69041b45974af6
penny t <pthongkhan1@my.scccd.edu>
2024-03-06 14:22:52 -0800

    ok

---
+++ 128/samples.sql
@@ -0,0 +1,4 @@
+-- SELECT * FROM PEOPLE;
+-- SELECT pfirstname, plastname, pstate, pcity FROM PEOPLE;
+SELECT pfirstname, plastname, pstate, pcity FROM PEOPLE WHERE ppoints > 250;
+-- SELECT pfirstname, plastname, pstate, pcity FROM PEOPLE WHERE pstate = 'NY' or pcity = 'Cincinnati';
