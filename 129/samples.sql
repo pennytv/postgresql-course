bc7808a989744c1317af79532a2058c5b9cc226d
penny t <pthongkhan1@my.scccd.edu>
2024-03-06 14:41:40 -0800

    ok

---
+++ 129/samples.sql
@@ -0,0 +1,5 @@
+-- SELECT * FROM PEOPLE;
+-- SELECT pfirstname, plastname, pstate, pcity FROM PEOPLE;
+-- SELECT pfirstname, plastname, ppoints FROM PEOPLE WHERE ppoints BETWEEN 250 AND 720;
+SELECT pfirstname, plastname, ppoints FROM PEOPLE WHERE ppoints >= 250 AND ppoints <= 720;
+-- SELECT pfirstname, plastname, pstate, pcity FROM PEOPLE WHERE pstate = 'NY' or pcity = 'Cincinnati';
---
+++ 131/samples.sql
@@ -0,0 +1,3 @@
+-- SELECT pfirstname, plastname, pstate, pcity FROM people WHERE pstate LIKE 'A_';
+-- SELECT pfirstname, plastname, pstate, pcity FROM people WHERE pcity LIKE 'A%';
+SELECT pfirstname, plastname, pstate, pcity FROM people WHERE pcity LIKE '%a%'
